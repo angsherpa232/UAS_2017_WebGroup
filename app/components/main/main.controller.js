291bfa219871a282ebeca0803f14e903252c0d8f
gebruwelay <al364012@uji.es>
2017-05-30 17:56:39 +0200

    Gebru's branch

--- app/components/main/main.controller.js
+++ app/components/main/main.controller.js
@@ -1,7 +1,7 @@
 (function () {
     'use strict';
     function MainCtrl($scope, $timeout) {
-		var imageLayer; //This is Gebru's repository
+		var imageLayer; //This is Gebru's repository.
 		var executed = false;
 		var ctrl = new L.LayerGroup();
 		var map;
