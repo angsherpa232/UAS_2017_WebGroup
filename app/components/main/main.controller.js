4a77a7921710a8126a7b6c427e3bc2b7f100b785
Nimrod Gavish <Nimrod Gavish>
2017-05-30 16:50:38 +0200

    Leaflet Main Implementation

--- app/components/main/main.controller.js
+++ app/components/main/main.controller.js
@@ -1,42 +1,51 @@
 (function () {
     'use strict';
     function MainCtrl($scope, $timeout) {
+		var imageLayer;
+		var executed = false;
+		var ctrl = new L.LayerGroup();
+		var map;
+		
+		$scope.createMap = function (){
 
-        angular.extend($scope, {
-            map: {
-                center: {
-                    lat: 51.95,
-                    lng: 7.6,
-                    zoom: 12
-                },
-                bounds: {
-                },
-                layers: {
-                    baselayers: {
-                        osm: {
-                            name: 'OpenStreetMap',
-                            url: 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
-                            type: 'xyz'
-                        },
-                        mapbox_light: {
-                            name: 'Mapbox Light',
-                            url: 'http://api.tiles.mapbox.com/v4/{mapid}/{z}/{x}/{y}.png?access_token={apikey}',
-                            type: 'xyz',
-                            layerOptions: {
-                                apikey: 'pk.eyJ1IjoibmF2ZWVuamFmZXIiLCJhIjoiY2lsYnVmamE0MDA1MXdnbHpvNGZianRuOCJ9.5KqDlJGBKr7ZF9Rdg6j_yQ',
-                                mapid: 'naveenjafer.0n3ooo76'
-                            }
-                        }
-                    }
-                },
-                defaults: {
-                    scrollWheelZoom: true,
-                    zoomControl: true,
-                    doubleClickZoom: true,
-                    dragging: true
-                }
-            }
-        });
+			var mbAttr = 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
+						'<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+						'Imagery © <a href="http://mapbox.com">Mapbox</a>',
+				mbUrl = 'https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibmdhdmlzaCIsImEiOiJjaXFheHJmc2YwMDdoaHNrcWM4Yjhsa2twIn0.8i1Xxwd1XifUU98dGE9nsQ';
+
+			var grayscale   = L.tileLayer(mbUrl, {id: 'mapbox.light', attribution: mbAttr}),
+				streets  = L.tileLayer(mbUrl, {id: 'mapbox.streets',   attribution: mbAttr}),
+				outdoors = L.tileLayer(mbUrl, {id: 'mapbox.outdoors', attribution: mbAttr}),
+				satellite = L.tileLayer(mbUrl, {id: 'mapbox.satellite', attribution: mbAttr}),
+				dark = L.tileLayer(mbUrl, {id: 'mapbox.dark', attribution: mbAttr}),
+				light = L.tileLayer(mbUrl, {id: 'mapbox.light', attribution: mbAttr}),
+				satellitestreets = L.tileLayer(mbUrl, {id: 'mapbox.streets-satellite', attribution: mbAttr});
+
+			map = L.map('mapid', {
+				center: [51.943703,7.573759], /*Default location */
+				zoom: 16, /*Default Zoom, Higher = Closer) */
+				layers: [satellite] // Default basemaplayer on startrup, can also give another layer here to show by default)
+			});
+
+			var baseLayers = {
+				"Grayscale": grayscale,
+				"Streets": streets,
+				"Outdoors": outdoors,
+				"Satellite": satellite,
+				"Satellite Streets": satellitestreets,
+				"Dark Map": dark,
+				"Light Map": light
+			};
+			ctrl = L.control.layers(baseLayers).addTo(map);
+
+		} //end of createMap Function
+        
+		$scope.createMap() 
+		
+		$scope.zoomRiver=function (){
+			map.setView([51.943703,7.573759], 16);
+		}
+		
         $timeout(function () {
             window.dispatchEvent(new Event('resize'))
         },
